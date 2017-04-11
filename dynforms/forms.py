b9a1df6694fc33d0f1b818b95f71d481670201f0
mic4ael <crossner90@gmail.com>
2017-04-11 22:25:26 +0200

    Make registration forms fields required

--- dynforms/forms.py
+++ dynforms/forms.py
@@ -8,5 +8,7 @@ class LoginForm(forms.Form):
 
 class RegistrationForm(forms.Form):
     username = forms.CharField(required=True)
-    password = forms.CharField(required=True)
+    password = forms.CharField(widget=forms.PasswordInput(), required=True)
     email = forms.EmailField(required=True)
+    first_name = forms.CharField(required=True)
+    last_name = forms.CharField(required=True)

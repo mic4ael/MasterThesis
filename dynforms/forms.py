cf44889078b808308f83d60b4cf0f7cae4ddd109
mic4ael <crossner90@gmail.com>
2017-04-23 20:02:01 +0200

    Make sure usernames are unique

--- dynforms/forms.py
+++ dynforms/forms.py
@@ -1,4 +1,8 @@
 from django import forms
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import EmailValidator
+from django.utils.translation import ugettext as _
 
 
 class LoginForm(forms.Form):
@@ -9,6 +13,11 @@ class LoginForm(forms.Form):
 class RegistrationForm(forms.Form):
     username = forms.CharField(required=True)
     password = forms.CharField(widget=forms.PasswordInput(), required=True)
-    email = forms.EmailField(required=True)
+    email = forms.EmailField(required=True, validators=[EmailValidator()])
     first_name = forms.CharField(required=True)
     last_name = forms.CharField(required=True)
+
+    def clean_username(self):
+        if User.objects.filter(username=self.cleaned_data['username']).exists():
+            raise ValidationError(_('Nazwa użytkownika jest już zajęta'))
+        return self.cleaned_data['username']

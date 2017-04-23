c4b752624b8ac5d6e06e9f5e2c324e8c4a5d03b8
mic4ael <crossner90@gmail.com>
2017-04-23 20:02:34 +0200

    Base idea for models

--- dynforms/models.py
+++ dynforms/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Form(models.Model):
+    pass
+
+
+class BaseFormField(models.Model):
+    pass
+
+
+class FormFieldAnswer(models.Model):
+    pass
